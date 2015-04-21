b4f19eac7759c0dcb31cedfae44303d1c9dfc19e
Stephen Balkum <stephen@balkum.com>
2015-04-21 14:49:24 -0500

    Updated dependencies Moq and StructureMap. Updated nuget package generation.

--- src/SimpleCqrs/Properties/AssemblyInfo.cs
+++ src/SimpleCqrs/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleCqrs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A framework for building applications using the CQRS and Event Sourcing patterns.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Tyrone Groves,Darren Cauthen")]
 [assembly: AssemblyProduct("SimpleCqrs")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © 2011 Tyrone Groves")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
